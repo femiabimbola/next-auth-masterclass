9fc1b00211c2531f965f6990612c18d4778bd4ad
Femi Abimbola <femiabimbola6@gmail.com>
2024-01-10 20:11:17 -0800

    added the card

---
+++ components/ui/auth/card-wrapper.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+interface CardWrapperProps {
+  children: React.ReactNode;
+  headerLabel: string;
+  backButtonLabel: string;
+  backButtonHref: string;
+}
+
+const CardWrapper = () => {
+  return <div>card wrapper</div>;
+};
+
+export default CardWrapper;
