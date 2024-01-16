698de677794cf6395c105e55233d4104ec895546
Femi Abimbola <femiabimbola6@gmail.com>
2024-01-16 11:04:41 -0800

    updating the login form

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -13,6 +13,8 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import {Input} from "@/components/ui/input";
+import {Button} from "@/components/ui/button";
 
 const LoginForm = () => {
   const form = useForm<z.infer<typeof LoginSchema>>({
@@ -28,7 +30,45 @@ const LoginForm = () => {
       showSocial
     >
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(() => {})}></form>
+        <form onSubmit={form.handleSubmit(() => {})} className="space-y-6">
+          <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({field}) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="Enter your email"
+                      type="email"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({field}) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="Enter your password"
+                      type="password"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button> Submit</Button>
+        </form>
       </Form>
     </CardWrapper>
   );
--- schemas/index.ts
+++ schemas/index.ts
@@ -1,6 +1,6 @@
 import * as z  from 'zod'
 
 export const LoginSchema = z.object({
-  email: z.string().email(),
+  email: z.string().email({message: "Valid email is required"}),
   password: z.string()
 })
