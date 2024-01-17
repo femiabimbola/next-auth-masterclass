31039783c6a248a81e443b4017d70d81dedbd6db
Femi Abimbola <femiabimbola6@gmail.com>
2024-01-17 09:44:02 -0800

    added form error

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -22,6 +22,10 @@ const LoginForm = () => {
     defaultValues: {email: "", password: ""},
   });
 
+  const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+    console.log(values);
+  };
+
   return (
     <CardWrapper
       headerLabel="Welcome back"
@@ -30,7 +34,7 @@ const LoginForm = () => {
       showSocial
     >
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(() => {})} className="space-y-6">
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
           <div className="space-y-4">
             <FormField
               control={form.control}
@@ -67,7 +71,9 @@ const LoginForm = () => {
               )}
             />
           </div>
-          <Button> Submit</Button>
+          <Button type="submit" className="w-full">
+            Login
+          </Button>
         </form>
       </Form>
     </CardWrapper>
---
+++ components/form-error.tsx
@@ -0,0 +1,11 @@
+import {ExclamationTriangleIcon} from "@radix-ui/react-icons";
+
+interface FormErrorProps {
+  message?: string;
+}
+
+const FormError = ({message}: FormErrorProps) => {
+  return <div>error</div>;
+};
+
+export default FormError;
