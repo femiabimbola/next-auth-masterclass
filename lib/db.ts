11eee8bee575396bad8af8f3d59f9193efa68dbd
Femi Abimbola <femiabimbola6@gmail.com>
2024-01-26 21:53:22 -0800

    adding Prisma

---
+++ lib/db.ts
@@ -0,0 +1,17 @@
+// To prevent too many prisma client, we have to add store the database in  globalThis.prisma for the development mode only
+// globalThis is not affected by hotreload
+
+// Prisma Learning, do this before doing Prisma init, after add this, do Prisma init
+
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+export const db = globalThis.prisma || new PrismaClient()
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = db
+
+
+// export const db = new PrismaClient(); it is for production
