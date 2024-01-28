18f5a083ef0faa7ea2a5c5ccc6a7f6bd2076f396
Femi Abimbola <femiabimbola6@gmail.com>
2024-01-28 01:04:42 -0800

    updated the register server

--- actions/register.ts
+++ actions/register.ts
@@ -1,14 +1,17 @@
 "use server"
 
 import * as z from 'zod'
+import bcrypt from 'bcrypt'
 
 import { RegisterSchema } from '@/schemas'
 
 export const Register = async (values: z.infer<typeof RegisterSchema >) => {
+
   const validatedFields = await RegisterSchema .safeParse(values)
-  
-  if(!validatedFields.success) return{ error: "Invalid fields"}
+  if(!validatedFields.success) return { error: "Invalid fields"}
   // For API route, you should use Nextresponse.json
+
+  const {password, email, name } = validatedFields.data
 
   return { success: "Email Sent!"}
   
