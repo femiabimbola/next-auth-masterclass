d730005bc265eb0c778d18717a7d146056e09e47
Femi Abimbola <femiabimbola6@gmail.com>
2024-01-28 02:30:27 -0800

    setting up prisma data for register

--- actions/register.ts
+++ actions/register.ts
@@ -2,17 +2,29 @@
 
 import * as z from 'zod'
 import bcrypt from 'bcrypt'
+import { db } from '@/lib/db'
+import { getUserByEmail } from '@/data/user'
 
 import { RegisterSchema } from '@/schemas'
 
 export const Register = async (values: z.infer<typeof RegisterSchema >) => {
 
-  const validatedFields = await RegisterSchema .safeParse(values)
+  const validatedFields = await RegisterSchema.safeParse(values)
   if(!validatedFields.success) return { error: "Invalid fields"}
   // For API route, you should use Nextresponse.json
 
-  const {password, email, name } = validatedFields.data
+  const {password, email, name } = validatedFields.data;
 
-  return { success: "Email Sent!"}
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  // const existingUser = await db.user.findUnique({where: {email}})
+
+  const existingUser = await getUserByEmail(email)
+
+  if(existingUser) return {error: "Email already in use"}
+
+  await db.user.create({ data: {name, email, password: hashedPassword}})
+
+  return { success: "User created successfully"}
   
 }
