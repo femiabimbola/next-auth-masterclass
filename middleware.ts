6a5258952d3c4e46abee67b35faaab938b6f9f37
Femi Abimbola <femiabimbola6@gmail.com>
2024-02-01 12:11:19 -0800

    updated the middleware

--- middleware.ts
+++ middleware.ts
@@ -2,9 +2,25 @@ import authConfig from "@/auth.config"
 import NextAuth from "next-auth"
 
 const {auth } = NextAuth(authConfig)
+import { apiAuthPrefix, DEFAULT_LOGIN_REDIRECT, authRoutes, publicRoutes } from "@/routes"
+
 
 export default auth((req) => {
-  console.log(req.nextUrl.pathname)
+  const pathname = req.nextUrl
+  const {nextUrl} = req;
+  const isLoggedIn = !!req.auth;
+  // console.log(req.nextUrl.pathname)
+
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix)
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname)
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname)
+
+  if(isApiAuthRoute) return null
+
+  if(isAuthRoute) {
+    if(isLoggedIn) return Response.redirect(new URL('DEFAULT_LOGIN_REDIRECT'))
+    return null
+  }
 })
 
 // Optionally, don't invoke Middleware on some paths
--- routes.ts
+++ routes.ts
@@ -1,7 +1,32 @@
 /**
  * An array of route that are accessible to the public
+ * They do not require authentication
+ * @type{string[]}
  */
-
 export const publicRoutes = [
+"/"
+];
 
-]
+/**
+ * The route used for authentication
+ * These routes will redirect logged in users to /settings
+ * @type{string[]}
+ */
+export const authRoutes = [
+  "/auth/login",
+  "/auth/register"
+];
+
+
+/**
+ * The prefix for API authentication
+ * Routes that start with this prefix are used for API authentication
+ * @type{string}
+ */
+export const apiAuthPrefix = "/api/auth";
+
+/**
+ * The default redirect part after logging in
+ * @type{string}
+ */
+export const DEFAULT_LOGIN_REDIRECT = "/settings";
