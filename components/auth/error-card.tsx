ee6283385560e5d86aebd08deb725c1874e52e6f
Femi Abimbola <femiabimbola6@gmail.com>
2024-02-10 10:43:43 -0800

    updated the auth page

--- components/auth/error-card.tsx
+++ components/auth/error-card.tsx
@@ -3,7 +3,17 @@ import BackButton from "@/components/auth/back-button";
 import {Card, CardFooter, CardHeader} from "@/components/ui/card";
 
 const ErrorCard = () => {
-  <div> The error card </div>;
+  <Card className="w-[400px] shadow-md">
+    <CardHeader>
+      <Header label="Ooops! Something went wrong" />
+    </CardHeader>
+    <CardFooter>
+      <BackButton
+        href="/auth/login"
+        label="Back to login"
+      />
+    </CardFooter>
+  </Card>;
 };
 
 export default ErrorCard;
