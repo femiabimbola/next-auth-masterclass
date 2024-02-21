b43f7d903e327b2001df8201845bb3fea49e52bb
Femi Abimbola <femiabimbola6@gmail.com>
2024-02-20 16:22:26 -0800

    client side of the verification email

--- components/auth/new-verification.tsx
+++ components/auth/new-verification.tsx
@@ -1,17 +1,33 @@
 "use client";
-
 import {BeatLoader} from "react-spinners";
-import {useCallback, useEffect} from "react";
+import {useCallback, useEffect, useState} from "react";
 import CardWrapper from "@/components/auth/card-wrapper";
 import {useSearchParams} from "next/navigation";
+import {newVerification} from "@/actions/new-verification";
+import FormError from "@/components/form-error";
+import FormSuccess from "@/components/form-success";
 
 // How to get the token from the url
 
 const NewVerification = () => {
+  const [error, setError] = useState<string | undefined>("");
+  const [success, setSuccess] = useState<string | undefined>();
   const searchParams = useSearchParams();
   const token = searchParams.get("token");
+
   const onSubmit = useCallback(() => {
-    console.log(token);
+    if (!token) {
+      setError("No Token found");
+      return;
+    }
+    newVerification(token)
+      .then((data) => {
+        setSuccess(data.success);
+        setError(data.error);
+      })
+      .catch(() => {
+        setError("Something went wrong!");
+      });
   }, [token]);
 
   useEffect(() => {
@@ -25,8 +41,9 @@ const NewVerification = () => {
       backButtonLabel=" Back to login"
     >
       <div className="flex items-center w-full justify-center">
-        <BeatLoader />
-        <p>{token}</p>
+        {!success && !error && <BeatLoader />}
+        <FormSuccess message={success} />
+        <FormError message={error} />
       </div>
     </CardWrapper>
   );
