86c8b1c76c9c0de650121d716995a4c89738bb39
Femi Abimbola <femiabimbola6@gmail.com>
2024-03-10 13:53:09 -0700

    adding custom hook

--- app/(protected)/csetting/page.tsx
+++ app/(protected)/csetting/page.tsx
@@ -1,15 +1,25 @@
 "use client";
 
-// It requires session provider
+// It requires session provider.
+// You can create a layout file in the protected folder
+
 import {useSession} from "next-auth/react";
+import {logout} from "@/actions/logout";
 
 const SettingsPage = () => {
   const session = useSession();
+  const onClick = () => logout();
   return (
     <div>
       {JSON.stringify(session)}
       <form>
-        <button type="submit"> Sign Out</button>
+        <button
+          onClick={onClick}
+          type="submit"
+        >
+          {" "}
+          Sign Out
+        </button>
       </form>
     </div>
   );
