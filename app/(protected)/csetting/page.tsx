15ec2bb43e1272c62931267c7bda3a1c72612e3e
Femi Abimbola <femiabimbola6@gmail.com>
2024-03-11 16:05:38 -0700

    adding layout to the protected layout

--- app/(protected)/csetting/page.tsx
+++ app/(protected)/csetting/page.tsx
@@ -3,24 +3,23 @@
 // It requires session provider.
 // You can create a layout file in the protected folder
 
-import {useSession} from "next-auth/react";
+// import {useSession} from "next-auth/react";
 import {logout} from "@/actions/logout";
+import {useCurrentUser} from "@/hooks/use-current-user";
 
 const SettingsPage = () => {
-  const session = useSession();
+  // const session = useSession();
+  const user = useCurrentUser();
   const onClick = () => logout();
+
   return (
-    <div>
-      {JSON.stringify(session)}
-      <form>
-        <button
-          onClick={onClick}
-          type="submit"
-        >
-          {" "}
-          Sign Out
-        </button>
-      </form>
+    <div className="bg-white p-10 roundex-xl">
+      <button
+        onClick={onClick}
+        type="submit"
+      >
+        Sign Out
+      </button>
     </div>
   );
 };
---
+++ app/(protected)/layout.tsx
@@ -0,0 +1,5 @@
+const ProtectedLayout = () => {
+  return <div></div>;
+};
+
+export default ProtectedLayout;
