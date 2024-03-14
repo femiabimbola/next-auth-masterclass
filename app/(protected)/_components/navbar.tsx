682208acdeb92016810a8269c5afb59be7799a4c
Femi Abimbola <femiabimbola6@gmail.com>
2024-03-14 15:55:18 -0700

    worked on navbar for protected

--- app/(protected)/_components/navbar.tsx
+++ app/(protected)/_components/navbar.tsx
@@ -8,7 +8,32 @@ export const Navbar = () => {
   const pathname = usePathname();
   return (
     <nav className="bg-secondary flex justify-between item-center p-4 rounded-xl w-[600px] shadow-sm">
-      <div className="flex gap-x-2"></div>
+      <div className="flex gap-x-2">
+        <Button
+          asChild
+          variant={pathname === "/server" ? "default" : "outline"}
+        >
+          <Link href={"/server"}>Server</Link>
+        </Button>
+        <Button
+          asChild
+          variant={pathname === "/client" ? "default" : "outline"}
+        >
+          <Link href={"/client"}>client</Link>
+        </Button>
+        <Button
+          asChild
+          variant={pathname === "/admin" ? "default" : "outline"}
+        >
+          <Link href={"/admin"}>Admin</Link>
+        </Button>
+        <Button
+          asChild
+          variant={pathname === "/settings" ? "default" : "outline"}
+        >
+          <Link href={"/settings"}>Settings</Link>
+        </Button>
+      </div>
       <p>User button</p>
     </nav>
   );
