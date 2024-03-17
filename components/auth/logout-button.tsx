0c5d6bed01e39c1de25a3a99f3b4531fe216694f
Femi Abimbola <femiabimbola6@gmail.com>
2024-03-16 23:22:19 -0700

    adding dropdown menu and avatar

--- components/auth/logout-button.tsx
+++ components/auth/logout-button.tsx
@@ -1,15 +1,26 @@
 "use client";
 
 import {logout} from "@/actions/logout";
+import {signOut} from "next-auth/react";
 
 interface LogoutButtonProps {
   children?: React.ReactNode;
 }
 
-export const LogotButton = ({children}: LogoutButtonProps) => {
+export const LogoutButton = ({children}: LogoutButtonProps) => {
   {
     const onClick = () => {
       logout();
+      // signOut();
     };
+
+    return (
+      <span
+        onClick={onClick}
+        className="cursor-pointer"
+      >
+        {children}
+      </span>
+    );
   }
 };
