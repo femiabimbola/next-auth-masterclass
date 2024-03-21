55fe52551a153614322e579821fe330e9b4e046e
Femi Abimbola <femiabimbola6@gmail.com>
2024-03-21 15:04:57 -0700

    server editing

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -2,7 +2,7 @@ import {Navbar} from "./_components/navbar";
 
 const ProtectedLayout = ({children}: {children: React.ReactNode}) => {
   return (
-    <div className="h-full flex items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+    <div className="h-full gap-y-10 flex flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
       <Navbar />
       {children}
     </div>
--- app/(protected)/server/page.tsx
+++ app/(protected)/server/page.tsx
@@ -2,7 +2,7 @@ import {auth} from "@/auth";
 
 const ServerPage = async () => {
   const session = await auth();
-  return <div>{JSON.stringify(session)}</div>;
+  return <div className="mx-auto">{JSON.stringify(session)}</div>;
 };
 
 export default ServerPage;
