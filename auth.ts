f77e3f57f3ee4f582632e42848fb27c47bbd2a59
Femi Abimbola <femiabimbola6@gmail.com>
2024-03-24 22:58:44 -0700

    updating the auth logic

--- auth.ts
+++ auth.ts
@@ -4,14 +4,14 @@
  * that only works with this time of file
  */
 
-import NextAuth, {DefaultSession, Session} from "next-auth"
-import authConfig from "@/auth.config"
-import { getUserById } from "@/data/user"
-import { UserRole } from "@prisma/client"
-import { db } from "@/lib/db"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import { JWT } from "next-auth/jwt"
-import { getTwoFactorConfirmationByUserId } from "@/data/two-factor-confirmation"
+import NextAuth, {DefaultSession, Session} from "next-auth";
+import authConfig from "@/auth.config";
+import {getUserById} from "@/data/user";
+import {UserRole} from "@prisma/client";
+import {db} from "@/lib/db";
+import {PrismaAdapter} from "@auth/prisma-adapter";
+import {JWT} from "next-auth/jwt";
+import {getTwoFactorConfirmationByUserId} from "@/data/two-factor-confirmation";
 
 // declare module "@auth/core" {
 //   interface Session {
@@ -23,72 +23,88 @@ import { getTwoFactorConfirmationByUserId } from "@/data/two-factor-confirmation
 
 // auth, signIn  and signOut can only be used in server components or server actions
 
-export const { handlers: { GET, POST },
- auth, signIn, signOut,
-} = NextAuth({ 
+export const {
+  handlers: {GET, POST},
+  auth,
+  signIn,
+  signOut,
+} = NextAuth({
   //  It is for using your own generated pages
   // It is located in the app folder
   pages: {
     signIn: "/auth/login",
-    error: "/auth/error"
+    error: "/auth/error",
   },
   events: {
     // for email verifing for oAuth sign in
-    async linkAccount({ user }){
+    async linkAccount({user}) {
       await db.user.update({
         where: {id: user.id},
-        data: { emailVerified: new Date()}
-      })
-    }
+        data: {emailVerified: new Date()},
+      });
+    },
   },
 
   callbacks: {
-
     // Adding a field to the token
     async jwt({token}) {
-      if(!token.sub) return token;
-      const existingUser = await getUserById(token.sub)
-      if(!existingUser) return token
-      token.role = existingUser.role
+      if (!token.sub) return token;
+      const existingUser = await getUserById(token.sub);
+      if (!existingUser) return token;
+      token.role = existingUser.role;
+      token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled;
       return token;
     },
 
-    // You caan manipulate the session ID 
-    async session({ token , session} : any){
-      if( session.user&& token.sub){
-        session.user.id = token.sub
+    // You caan manipulate the session ID
+    async session({token, session}: any) {
+      if (session.user && token.sub) {
+        session.user.id = token.sub;
       }
-      
-      if(token.role && session.user){
-        session.user.role = token.role 
+
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
       }
+
+      // if (token.isTwoFactorEnabled && session.user) {
+      //   session.user.isTwoFactorEnabled = token.isTwoFactorEnabled;
+      // }
+
+      if (session.user) {
+        session.user.isTwoFactorEnabled = token.isTwoFactorEnabled;
+      }
+
       return session;
     },
 
     async signIn({user, account}) {
       // Allow OAuth without email verification
-      if(account?.provider !== "credentials") return true
+      if (account?.provider !== "credentials") return true;
 
       // Prevent sign in without email verification
-      const existingUser = await getUserById(user.id) 
-      if(!existingUser?.emailVerified) {return false}
+      const existingUser = await getUserById(user.id);
+      if (!existingUser?.emailVerified) {
+        return false;
+      }
 
       //  TODO: Add 2FA check
-      if(existingUser.isTwoFactorEnabled) {
-        const twoFactorConfirmation = await getTwoFactorConfirmationByUserId(existingUser.id)
+      if (existingUser.isTwoFactorEnabled) {
+        const twoFactorConfirmation = await getTwoFactorConfirmationByUserId(
+          existingUser.id
+        );
 
-       if(!twoFactorConfirmation) return false
+        if (!twoFactorConfirmation) return false;
 
-      //  Delete two factor confirmation for next sign in
-      await db.twoFactorConfirmation.delete({
-        where: {id: twoFactorConfirmation.id}
-      })
+        //  Delete two factor confirmation for next sign in
+        await db.twoFactorConfirmation.delete({
+          where: {id: twoFactorConfirmation.id},
+        });
       }
 
       return true;
-    }
+    },
   },
   adapter: PrismaAdapter(db),
   session: {strategy: "jwt"},
   ...authConfig,
-})
+});
--- components/user-info.tsx
+++ components/user-info.tsx
@@ -12,7 +12,43 @@ export const UserInfo = ({user, label}: UserInfoProps) => {
       <CardHeader>
         <p>{label}</p>
       </CardHeader>
-      <CardContent className="space-y-4"></CardContent>
+      <CardContent className="space-y-4">
+        <div className=" flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
+          <p className="text-sm font-medium">ID</p>
+          <p className="truncate text-xs max-w-[180px] font-mono p-1 bg-slate-100 rounded-md">
+            {user?.id}
+          </p>
+        </div>
+        {/* The second div */}
+        <div className=" flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
+          <p className="text-sm font-medium">Name</p>
+          <p className="truncate text-xs max-w-[180px] font-mono p-1 bg-slate-100 rounded-md">
+            {user?.name}
+          </p>
+        </div>
+        {/* The third div */}
+        <div className=" flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
+          <p className="text-sm font-medium">Email</p>
+          <p className="truncate text-xs max-w-[180px] font-mono p-1 bg-slate-100 rounded-md">
+            {user?.email}
+          </p>
+        </div>
+        {/* The fourth div */}
+        <div className=" flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
+          <p className="text-sm font-medium">Role</p>
+          <p className="truncate text-xs max-w-[180px] font-mono p-1 bg-slate-100 rounded-md">
+            {user?.role}
+          </p>
+        </div>
+        {/* The fifth div */}
+        <div className=" flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
+          <p className="text-sm font-medium">Two Factor Auth</p>
+          <p className="truncate text-xs max-w-[180px] font-mono p-1 bg-slate-100 rounded-md">
+            {/* {user?.role} */}
+            Off
+          </p>
+        </div>
+      </CardContent>
     </Card>
   );
 };
