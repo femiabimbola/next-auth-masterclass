e9acb21a59974c6257f387b61084df5026e6b511
Femi Abimbola <femiabimbola6@gmail.com>
2024-03-30 04:13:59 -0700

    added admin server action

--- app/(protected)/admin/page.tsx
+++ app/(protected)/admin/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import {admin} from "@/actions/admin";
 import RoleGate from "@/components/auth/role-gate";
 import FormSuccess from "@/components/form-success";
 import {Button} from "@/components/ui/button";
@@ -13,11 +14,23 @@ import {toast} from "sonner";
  */
 
 const AdminPage = () => {
+  const onServerActionClick = () => {
+    admin().then((data) => {
+      if (data.error) {
+        toast.error(data.error);
+      }
+
+      if (data.success) {
+        toast.error(data.success);
+      }
+    });
+  };
+
   const onApiRouteClick = () => {
     fetch("/api/admin").then((response) => {
       if (response.ok) toast.success("Allowed API route");
       else {
-        toast.error("error");
+        toast.error("Forbidden API route");
       }
     });
   };
@@ -37,7 +50,7 @@ const AdminPage = () => {
 
         <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-md">
           <p className="text-sm font-medium"> Admin-only Server Action</p>
-          <Button> Click to test</Button>
+          <Button onClick={onServerActionClick}> Click to test</Button>
         </div>
       </CardContent>
     </Card>
