7a5964fbbe894c5a2977e4c8fd5008b1266230c1
Femi Abimbola <femiabimbola6@gmail.com>
2024-04-02 20:51:04 -0700

    updated setting actions

--- actions/settings.ts
+++ actions/settings.ts
@@ -14,6 +14,14 @@ export const settings = async (values: z.infer<typeof SettingSchema>) => {
   const dbUser = await getUserById(user.id);
   if (!dbUser) return {error: "Unauthorized"};
 
+  // Should not be modified
+  if (user.isOAuth) {
+    values.email = undefined;
+    values.password = undefined;
+    values.newPassword = undefined;
+    values.isTwoFactorEnabled = undefined;
+  }
+
   await db.user.update({
     where: {id: dbUser.id},
     data: {...values},
